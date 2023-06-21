9d2c0cb241ad175643b30236ef7bfc64db85d62a
Amon <amon@cupist.com>
2023-06-21 14:59:45 +0900

    Initial Commit

---
+++ GreenField/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GreenField
+//
+//  Created by Amon on 2023/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GreenField/GreenFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenFieldApp.swift
+//  GreenField
+//
+//  Created by Amon on 2023/06/21.
+//
+
+import SwiftUI
+
+@main
+struct GreenFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
